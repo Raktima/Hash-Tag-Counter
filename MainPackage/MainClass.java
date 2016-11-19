7b5ff011db8622c3d5cef570fc880100647ace49
Raktima <raktimadas@ufl.edu>
2016-11-19 12:07:19 -0500

    Add files via upload

---
+++ MainPackage/MainClass.java
@@ -0,0 +1,23 @@
+package MainPackage; /**
+ * Created by Raktima on 11/5/2016.
+ */
+import Fibonacci.FibonacciHeap;
+import Input.*;
+
+import java.io.IOException;
+
+/**
+ * Created by Raktima on 11/4/2016.
+ */
+
+/* Main class used to test our input*/
+public class MainClass {
+
+    public static void main(String args[]) throws IOException {
+        ReadInput r=new ReadInput();
+        r.readSample();
+        /*FibonacciHeap fib=new FibonacciHeap();
+        fib.startImplementation();
+*/
+    }
+}
